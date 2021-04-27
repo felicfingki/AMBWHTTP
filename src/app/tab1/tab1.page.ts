ce829a38b38d4fc9a4a0a361a22de5cf714bc916
Felic <felicfingki@yahoo.com>
2021-04-27 20:23:28 +0700

    master

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,45 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private http : HttpClient,private loadCtrl : LoadingController) {}
+
+  dataPOST = [];
+
+  loading : any;
+
+  async getDataPost()
+  {
+    this.loading = await this.loaderPresent();
+
+    this.http.get("https://reqres.in/api/users?page=2").subscribe((res:any)=>{
+      console.log(res.data);
+      this.dataPOST = res.data;
+      if(this.loading)
+      {
+        this.loading.dismiss();
+      }
+    });
+  }
+
+  ionViewDidEnter()
+  {
+    this.getDataPost();
+  }
+
+  public async loaderPresent() : Promise<any> 
+  {
+    const loading = await this.loadCtrl.create({
+      message : "LOADING ...",
+      backdropDismiss : true
+    });
+    await loading.present();
+    
+    return loading;
+  }
+ 
+
+  
+
+
 
 }
